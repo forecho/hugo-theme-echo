e3b9b4643678662202a94b4569a063b7c985b78a
forecho <caizhenghai@gmail.com>
2021-04-02 16:45:58 +0800

    fix: js

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -2,7 +2,6 @@ const themeDir = __dirname + '/../../';
 
 module.exports = {
   plugins: [
-    require('postcss-import')({ path: [themeDir] }),
     require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
     require('autoprefixer')(),
   ]
